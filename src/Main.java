3c19b15b22982197c5fe905d1f5f9c7b24b6484d
Vladislav <M1LkMenIO@yandex.ru>
2026-05-30 19:23:10 +0300

    creating a successor class from the Thread class

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,15 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         System.out.println(Thread.currentThread().getName());
+
+        final Thread thread = new MyThread();
+        thread.start();
     }
 
     private static final class MyThread extends Thread{
         @Override
         public void run(){
-            
+            System.out.println(currentThread().getName());
         }
     }
 }
