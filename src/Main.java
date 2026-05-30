43550bc328a9bff0f09e93d1ea79fd99e409b3b5
Vladislav <M1LkMenIO@yandex.ru>
2026-05-30 19:31:01 +0300

    creating an anonymous descendant class from the Thread class

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 import java.util.List;
+import java.util.SortedMap;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println(Thread.currentThread().getName());
 
-        final Thread thread = new MyThread();
+        final Thread thread = new Thread(){
+            @Override
+            public void run (){
+                System.out.println(currentThread().getName());
+            }
+        };
         thread.start();
     }
 
-    private static final class MyThread extends Thread{
-        @Override
-        public void run(){
-            System.out.println(currentThread().getName());
-        }
-    }
 }
 
