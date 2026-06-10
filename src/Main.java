a28b89de1bca3f345011610906a2c7eb35b7ae0f
Vladislav <M1LkMenIO@yandex.ru>
2026-06-10 12:29:07 +0300

    Summation of numbers in two threads. Sleep and join methods

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,37 @@
 import static java.lang.Thread.currentThread;
 
 public class Main {
-    public static void main(String[] args) {
-        final Runnable task = () -> System.out.println(currentThread().getName());
-        final Thread thread = new Thread(task);
-        thread.start(); // вызов потока
+    private static final int FIRST_START_NUMBER = 1;
+    private static final int FIRST_END_NUMBER = 500;
+
+    private static final int SECOND_START_NUMBER = 501;
+    private static final int SECOND_END_NUMBER = 1000;
+    private static final String TEMPLATE_MESSAGE_THREAD_NAME_AND_NUMBER = "%s : %d\n";
+    private static final int TIME_WAITING_IN_MILLIS = 1000;
+
+
+    public static void main(String[] args)  throws InterruptedException {
+        final TaskSummingNumber firstTask =  new TaskSummingNumber(FIRST_START_NUMBER, FIRST_END_NUMBER);
+        final TaskSummingNumber secondTask = new TaskSummingNumber(SECOND_START_NUMBER, SECOND_END_NUMBER);
+        Thread firstThread = new Thread(firstTask);
+        Thread secondThread = new Thread(secondTask);
+        firstThread.start();
+        secondThread.start();
+
+        waitForTasksFinished(firstThread, secondThread);
+        final int resultNumber = firstTask.getSum() + secondTask.getSum();
+        System.out.printf(TEMPLATE_MESSAGE_THREAD_NAME_AND_NUMBER, currentThread().getName(), resultNumber);
+    }
+
+//    public static void startThread(final Runnable runnable){
+//        Thread thread = new Thread(runnable);
+//        thread.start();
+//    }
+
+    public static void waitForTasksFinished(final Thread... threads) throws InterruptedException{
+        for (Thread thread : threads) {
+            thread.join();
+        }
     }
 
 }
--- src/TaskSummingNumber.java
+++ src/TaskSummingNumber.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import java.util.stream.IntStream;
+import static java.lang.Thread.currentThread;
 
-public class TaskSummingNumber {
+public class TaskSummingNumber implements Runnable{
+
+    private final int startNumber;
+    private final int endNumber;
+    private int sum = 0;
+
+    public TaskSummingNumber(int startNumber, int endNumber){
+        this.startNumber = startNumber;
+        this.endNumber = endNumber;
+    }
+
+    public int getSum(){
+        return sum;
+    }
+
+    @Override
+    public void run(){
+        IntStream.rangeClosed(startNumber, endNumber).forEach(i -> sum += i);
+        System.out.println(currentThread().getName() + " : " + sum);
+    }
+
 }
